2293452b7a8965b74d7d17851dc6e89c8334cfae
Griko Nibras <git@griko.id>
2022-08-25 12:53:05 +0700

    chore(node/client): simplify tsup config

--- node/client/tsup.config.ts
+++ node/client/tsup.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "tsup";
 
-import { dependencies } from "./package.json";
-
 export default defineConfig(({ watch }) => ({
   clean: !watch,
   dts: true,
   entry: ["src/*.ts"],
-  external: [...Object.keys(dependencies)],
   format: ["cjs", "esm"],
   minify: !watch,
   minifyIdentifiers: !watch,
