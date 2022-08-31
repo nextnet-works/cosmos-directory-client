d614e0f950a2969ca22983f783a4fa2abcb50308
Griko Nibras <git@griko.id>
2022-08-31 13:56:59 +0700

    feat: use strangelove style guide

--- node/types/.eslintrc.js
+++ node/types/.eslintrc.js
@@ -1,12 +1,12 @@
 // @ts-check
 
-const { getTsconfigPath } = require("@grikomsn/style-guide/eslint/helpers");
+const { getTsconfigPath } = require("@strangelovelabs/style-guide/eslint/helpers");
 
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
   extends: [
-    require.resolve("@grikomsn/style-guide/eslint/node"),
-    require.resolve("@grikomsn/style-guide/eslint/typescript"),
+    require.resolve("@strangelovelabs/style-guide/eslint/node"),
+    require.resolve("@strangelovelabs/style-guide/eslint/typescript"),
   ],
   ignorePatterns: ["node_modules"],
   parserOptions: {
